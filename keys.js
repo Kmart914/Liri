3394bc8cd3164fa9dc4198e4a0f1559f6e5a270f
Kevin Martzolf <kmart914@gmail.com>
2017-08-24 19:16:18 -0700

    getting some spotify objects

--- keys.js
+++ keys.js
@@ -6,3 +6,12 @@ exports.twitterKeys = {
   access_token_key: '745321381785538560-e7LFQ2CG38Te80oPvHj5O2fnfcmCyYr',
   access_token_secret: 'u077LR25kvdXmjqUIImtaL597zvcyMprPDfRtWi9nExTM',
 }
+
+exports.spotifyKeys = {
+  id: "9f1caffd8a7d4ca8ad51c5636a1b0036",
+  secret: "7be7aa7358bb4131ac9643ba8e9f5921",
+}
+
+exports.omdbApi = {
+  id: "40e9cece",
+}
--- liri.js
+++ liri.js
@@ -1,19 +1,24 @@
-var twitter = require('twitter');
 var keys = require('./keys.js');
-
-console.log(client);
-
-var client = new twitter(keys.twitterKeys);
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
+var twitterClient = new twitter(keys.twitterKeys);
+var spotifyClient= new spotify(keys.spotifyKeys);
 
 
 if (process.argv[2] === "my-tweets"){
 var params = {screen_name: process.argv[3]};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
+twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
     for(var i = 0; i <20; i++){
     console.log(tweets[i].text);
       }
     }
   });
-}else {
+}else if (process.argv[2] === "spotify-this-song"){
+
+spotifyClient.search({ type: 'track', query: process.argv[3]}, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  } console.log(data);
+});
 }
