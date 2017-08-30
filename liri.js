fc8822828e9774a12e71f85526959d0c5ef4c11e
Kevin Martzolf <kmart914@gmail.com>
2017-08-29 17:35:18 -0700

    last update

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
 var keys = require('./keys.js');
 var twitter = require('twitter');
 var spotify = require('node-spotify-api');
+var request = require("request");
+var movieName = process.argv[2];
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
 var twitterClient = new twitter(keys.twitterKeys);
 var spotifyClient= new spotify(keys.spotifyKeys);
 
@@ -21,4 +25,11 @@ spotifyClient.search({ type: 'track', query: process.argv[3]}, function(err, dat
     return console.log('Error occurred: ' + err);
   } console.log(data);
 });
+}else if (process.argv[2] === "movie-this") {
+  request(queryUrl, function(err, response, body){
+    if (!err && response.statusCode === 200){
+      console.log("The movie was realeased in: " + JSON.parse(body).Year);
+    }
+  });
+
 }
